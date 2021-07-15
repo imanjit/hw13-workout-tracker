3d6bc609f0cb72c81874fed91a204eeafa02befc
Iman Bains <imanjitbains@yahoo.com>
2021-07-14 21:19:03 -0700

    Fixed error in server.js file

--- README.md
+++ README.md
@@ -6,7 +6,3 @@ NoSQL
 1. Run `npm start`
 2. In your browser, go to http://localhost:3010
 3. From here, you can log and store exercise details
-
-## Future Improvements 
-
-Find out why line 8 inside server.js breaks the code
--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const morgan = require("morgan");
 const app = express();
 const PORT = process.env.PORT || 3010
 
-// app.use(express.json);
+app.use(express.json());
 app.use(morgan("dev"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
